60c6d11187c561094827dcbd87cf62e6b027a3a6
OKLng <onkhoaluong2k7@gmail.com>
2026-03-29 11:51:08 +0700

    add struct Student, LinkNode, Class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Student{
+    string id;
+    string name;
+    float gpa;
+};
+
+struct Node{
+    Student Data;
+    Node* next;
+};
+
+class DanhSachQuanLy{
+private:
+    Node* head;
+
+public:
+    DanhSachQuanLy(){head = nullptr;}
+};
 
 int main(){
 
