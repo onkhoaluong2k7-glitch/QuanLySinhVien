c3ba2c853b23a35ee46e2b7be1fc51fb0ebe3081
OKLng <onkhoaluong2k7@gmail.com>
2026-03-29 12:07:57 +0700

    add DeleteStudent function

--- main.cpp
+++ main.cpp
@@ -25,8 +25,31 @@ public:
         newNode->Data = st;
         newNode->next = head;
         head = newNode;
-    }
+    }  
 
+    bool DeleteStudent( string id){
+        if (head == nullptr) return false;
+        // In case the node needs to be delete at the head
+        if (head->Data.id == id){
+            Node *tmp = head;
+            head = head->next;
+            delete tmp; // release memory
+            return true;
+        }
+        // In case the node needs to be delete at middle or the end
+        Node* curr = head;
+        Node* prev = nullptr;
+        while ( curr != nullptr && curr->Data.id != id){
+            prev = curr;
+            curr = curr->next;
+        }
+        // Not found the node that needs to be delete
+        if (curr == nullptr) return false;
+        // Found the node to be delete
+        prev->next = curr->next;
+        delete curr;
+        return true;
+    }
 };
 
 int main(){
