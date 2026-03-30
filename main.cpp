f74385545f8e81b3ddce65806086e7e31015d61d
OKLng <onkhoaluong2k7@gmail.com>
2026-03-30 10:29:47 +0700

    them ham Input, Output, Huyclass, them chuc nang cho ham

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <c
 using namespace std;
 
 struct Student{
     string id;
     string name;
     float gpa;
+
 };
 
 struct Node{
@@ -65,9 +67,61 @@ public:
         if (curr == nullptr) return false;// Not found
         return true;// Founded
     }
+    
+    bool isempty(){
+        return head == nullptr;
+    }
+
+    void InputStudent(){
+        Student a;
+        cout << "Nhap ID:";
+        cin >> a.id;
+        cin.ignore();
+        cout << "Nhap ten sinh vien :";
+        getline(cin, a.name);
+        cout << "Nhap gpa(chua co nhap 0.0): ";
+        cin >> a.gpa;
+        AddStudent(a);
+    }
+
+    void OutputStudent(){
+        Node *curr = head;
+        while (curr != nullptr){
+            cout << curr->Data.id << " " << curr->Data.name << " " << curr->Data.gpa << endl; 
+            curr = curr->next;
+        }
+    }
+    ~DanhSachQuanLy(){
+        while (head != nullptr){
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
 };
 
 int main(){
-
+    DanhSachQuanLy st;
+    int option;
+    do{
+        cout << "\n============= Menu Quan Ly =============" << endl;
+        cout << "0. Thoat he thong."<< endl;
+        cout << "1. Them Sinh Vien." << endl;
+        cout << "2. Hien thi danh sach." << endl;
+        switch (option){
+            case 1:
+                st.InputStudent();
+                break;
+            case 2:
+                st.OutputStudent();
+                break;
+            case 0:
+                cout << "Dang thoat ..."<< endl;
+                break;
+            default :
+                cout << "Lua chon khong hop le!!!" << endl;
+                break;
+        }
+    } while (option != 0) ;
     return 0;
 }
